8f280f79f5ba656095f7ed48385432e08079998b
RayFian <ray.fian7886@gmail.com>
2023-02-24 15:01:21 +0700

    first commit

---
+++ resources/views/week2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravek Week 1</title>
+        <style>
+            h1 {
+                
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
